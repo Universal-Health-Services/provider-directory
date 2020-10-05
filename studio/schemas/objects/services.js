1e4fd42657ae270c55c9902b49907490267a0e7f
Scudder Stevens <53576686+scudderuhsinc@users.noreply.github.com>
2020-10-05 17:34:32 -0400

    Update and rename providerServices.js to services.js

---
+++ studio/schemas/objects/services.js
@@ -0,0 +1,17 @@
+export default {
+    title: 'Service(s)',
+    name: 'services',
+    type: 'object',
+    fields: [
+        {
+            title: 'Scheduling ID',
+            name: 'dSID',
+            type: 'string'
+        },
+        {
+            title: 'Tracking ID',
+            name: 'dTID',
+            type: 'string'
+        }
+    ],
+  }

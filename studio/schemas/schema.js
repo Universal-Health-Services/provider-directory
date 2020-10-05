c55ace1eb949752313fbd75d600bc00b20eb362e
Scudder Stevens <53576686+scudderuhsinc@users.noreply.github.com>
2020-10-05 17:39:10 -0400

    Update schema.js

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -1,6 +1,4 @@
-// First, we must import the schema creator
 import createSchema from 'part:@sanity/base/schema-creator'
-// Then import schema types from any plugins that might expose them
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 
 // Documents types
@@ -8,21 +6,18 @@ import location from './documents/location'
 import provider from './documents/provider'
 import role from './documents/role'
 import specialty from './documents/specialty'
-import vertical from './documents/vertical'
 
 // Object types
 import body from './objects/bodyPortableText'
 import locationImage from './objects/locationImage'
 import profileImage from './objects/profileImage'
-import locationServices from './objects/locationServices'
-import providerServices from './objects/providerServices'
 
-// Then we give our schema to the builder and provide the result to Sanity
+// Service Objects
+import docasapProvider from './services/docasapProvider'
+import kyruusProvider from './services/kyruusProvider'
+
 export default createSchema({
-  // We name our schema
   name: 'default',
-  // Then proceed to concatenate our document type
-  // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
     location,
     provider,
@@ -31,8 +26,7 @@ export default createSchema({
     body,
     locationImage,
     profileImage,
-    locationServices,
-    providerServices,
-    vertical
+    docasapProvider,
+    kyruusProvider,
   ])
 })
